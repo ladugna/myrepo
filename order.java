6e70c230828db73870b651aff2e1aadd3f27e523
LEMESSA ADUGNA <abdibori52@gmail.com>
2023-01-20 19:58:02 -0600

    order file added

---
+++ order.java
@@ -0,0 +1,9 @@
+// this is my java
+class myclass{
+    public static String myrepo(){
+        return null;
+    }
+    public static void main (String[] args ){
+       System.ou.println(myrepo);
+    }
+}
